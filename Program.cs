6baa5a862a78bec41b05cc2218788b85bd1e72ea
Jake Dusing <dusingjake@gmail.com>
2025-04-23 06:32:41 -0500

    add Enemy class

--- Program.cs
+++ Program.cs
@@ -28,4 +28,23 @@ namespace FourthWingRPG
             Console.WriteLine($"{Name}'s Stats: \nHealth: {Health} \nMana: {Mana} \nAttack: {Attack} \nDefense: {Defense} \nLevel: {Level} \nXP: {XP}");
         }
     }
+
+    class Enemy
+    {
+        public string Name { get; set;}
+        public int Health { get; set; }
+        public int Attack { get; set; }
+
+        public Enemy(string name, int health, int attack)
+        {
+            Name = name;
+            Health = health;
+            Attack = attack;
+        }
+
+        public void AttackPlayer(Player player)
+        {
+            Console.WriteLine($"{Name} attacks you for {Attack} damage!");
+        }
+    }
 }
